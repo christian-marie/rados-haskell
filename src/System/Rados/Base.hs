efcae9a89442beb74d2a3d9386eab96942e5aae6
Christian Marie <pingu@anchor.net.au>
2013-12-12 12:18:08 +1100

    Very poorly worded and alpha documentation for internal Base.hs

--- src/System/Rados/Base.hs
+++ src/System/Rados/Base.hs
@@ -1,17 +1,29 @@
+-- |
+-- The underlying Base methods used for the monadic implementation
+-- "System.Rados.Monadic".
+--
+-- It is not recommended to use this module directly.
 module System.Rados.Base
 (
+-- *Type definitions
+    ClusterHandle,
+    Completion,
+-- *Connecting
     newClusterHandle,
     confReadFile,
     connect,
+-- *Writing
     newIOContext,
+-- **Synchronous
+    syncWrite,
+    syncRead,
+-- **Asynchronous
     newCompletion,
     waitForComplete,
     waitForSafe,
     isSafe,
     isComplete,
     asyncWrite,
-    syncWrite,
-    syncRead,
 ) where
 
 import qualified System.Rados.FFI as F
@@ -29,15 +41,21 @@ type IOContext     = ForeignPtr F.RadosIOCtxT
 type Completion    = ForeignPtr F.RadosCompletionT
 
 -- |
--- Attempt to create a new ClusterHandle, taking an optional id.
+-- Attempt to create a new 'ClusterHandle', taking an optional id.
 --
--- Calls rados_create:
--- http://ceph.com/docs/master/rados/api/librados/#rados_create
+-- @
+-- h  <- newClusterHandle Nothing
+-- h' <- newClusterHandle \"admin\"
+-- @
 --
--- The ClusterHandle returned will have rados_shutdown run when it is garbage
--- collected.
--- Calls rados_shutdown:
--- http://ceph.com/docs/master/rados/api/librados/#rados_shutdown
+-- Calls:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_create>
+--
+-- The ClusterHandle returned will have rados_shutdown automatically run when
+-- it is garbage collected.
+--
+-- Cleans up with:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_shutdown>
 newClusterHandle :: Maybe B.ByteString -> IO (ClusterHandle)
 newClusterHandle maybe_bs = do
     -- Allocate a void pointer to cast to our Ptr RadostT
@@ -52,12 +70,15 @@ newClusterHandle maybe_bs = do
     newForeignPtr F.c_rados_shutdown =<< peek radost_t_ptr
 
 -- |
--- Configure a ClusterHandle from a config file.
+-- Load a config specified by 'FilePath' into a given 'ClusterHandle'.
 --
--- Will load a config specified by FilePath into ClusterHandle.
+-- @
+-- h <- newClusterHandle Nothing
+-- confReadFile h \"/etc/config\"
+-- @
 --
--- Calls rados_conf_read_file:
--- http://ceph.com/docs/master/rados/api/librados/#rados_conf_read_file
+-- Calls:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_conf_read_file>
 confReadFile :: ClusterHandle -> FilePath -> IO ()
 confReadFile handle fp = void $
     withForeignPtr handle $ \rados_t_ptr ->
@@ -65,24 +86,35 @@ confReadFile handle fp = void $
             F.c_rados_conf_read_file rados_t_ptr cstr
 
 -- |
--- Attempt to connect a configured ClusterHandle.
+-- Attempt to connect a configured 'ClusterHandle'.
 --
--- Calls rados_connect
--- http://ceph.com/docs/master/rados/api/librados/#rados_connect
+-- @
+-- h <- newClusterHandle Nothing
+-- confReadFile h \"/etc/config\"
+-- connect h
+-- @
+--
+-- Calls: 
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_connect>
 connect :: ClusterHandle -> IO ()
 connect handle = void $ 
     withForeignPtr handle $ \rados_t_ptr ->
         checkError "c_rados_connect" $ F.c_rados_connect rados_t_ptr
 
 -- |
--- Attempt to create a new IOContext, requires a valid ClusterHandle and pool
--- name.
+-- Attempt to create a new 'IOContext', requires a valid 'ClusterHandle' and
+-- pool name.
 --
--- Calls c_rados_ioctx_create:
--- http://ceph.com/docs/master/rados/api/librados/#rados_ioctx_create
+-- @
+-- h <- newClusterHandle Nothing
+-- h ctx <- newIOContext h "thing"
+-- @
 --
--- Calls c_rados_ioctx_destroy on garbage collection:
--- http://ceph.com/docs/master/rados/api/librados/#rados_ioctx_destroy
+-- Calls:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_ioctx_create>
+--
+-- And on cleanup:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_ioctx_destroy>
 newIOContext :: ClusterHandle -> B.ByteString -> IO (IOContext)
 newIOContext handle bs = B.useAsCString bs $ \cstr -> do
     withForeignPtr handle $ \rados_t_ptr -> do
@@ -93,13 +125,14 @@ newIOContext handle bs = B.useAsCString bs $ \cstr -> do
 
 -- |
 -- Attempt to create a new completion that can be used with async IO actions.
+--
 -- This completion will be released automatically when it is garbage collected.
 --
--- Calls rados_aio_create_completion:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_create_completion
+-- Calls:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_create_completion>
 --
--- And rados_aio_release on cleanup:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_release
+-- And on cleanup:
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_release>
 newCompletion :: IO Completion
 newCompletion = do
     completion_ptr <- castPtr <$> (malloc :: IO (Ptr WordPtr))
@@ -111,8 +144,16 @@ newCompletion = do
 -- Block until a completion is complete. I.e. the operation associated with
 -- the completion is at least in memory on all replicas.
 --
+-- @
+-- ...
+-- asyncWrite ctx c "oid" 42 \"written at offset fourty-two\"
+-- putStrLn \"Waiting for your bytes to get there...\"
+-- waitForComplete c
+-- putStrLn \"I totally wrote it! Maybe.\"
+-- @
+--
 -- Calls rados_aio_wait_for_complete:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_wait_for_complete
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_wait_for_complete>
 waitForComplete :: Completion -> IO ()
 waitForComplete completion = void $
     withForeignPtr completion $ \rados_completion_t_ptr ->
@@ -122,8 +163,14 @@ waitForComplete completion = void $
 -- Block until a completion is safe. I.e. the operation associated with
 -- the completion is on stable storage on all replicas.
 --
+-- @
+-- ...
+-- waitForSafe c
+-- putStrLn \"Yeah. Totally did write it. I hope.\"
+-- @
+--
 -- Calls rados_aio_wait_for_safe:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_wait_for_safe
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_wait_for_safe>
 waitForSafe :: Completion -> IO ()
 waitForSafe completion = void $
     withForeignPtr completion $ \rados_completion_t_ptr ->
@@ -132,8 +179,10 @@ waitForSafe completion = void $
 -- |
 -- Is the operation associated with this completion in memory on all replicas?
 --
+--
+--
 -- Cals rados_aio_is_complete:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_is_complete
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_is_complete>
 isComplete :: Completion -> IO (Bool)
 isComplete completion = withForeignPtr completion $ \rados_completion_t_ptr ->
         (/= 0) <$> F.c_rados_aio_is_complete rados_completion_t_ptr
@@ -143,20 +192,26 @@ isComplete completion = withForeignPtr completion $ \rados_completion_t_ptr ->
 -- replicas?
 --
 -- Calls rados_aio_is_safe:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_is_safe
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_is_safe>
 isSafe :: Completion -> IO (Bool)
 isSafe completion = withForeignPtr completion $ \rados_completion_t_ptr ->
         (/= 0) <$> F.c_rados_aio_is_safe rados_completion_t_ptr
 
 -- |
 -- From right to left, this function reads as:
--- "Write ByteString bytes to Word64 offset at oid ByteString, notifying this
--- Completion, all within this IOContext"
 --
--- Returns the number of bytes written.
+-- Write ByteString bytes to Word64 offset at oid ByteString, notifying this
+-- Completion, all within this IOContext
+--
+-- @
+-- ...
+-- ctx <- newIOContext h \"thing\"
+-- n <- asyncWrite ctx c "oid" 42 \"written at offset fourty-two\"
+-- putStrLn $ "I wrote " ++ show n ++ " bytes"
+-- @
 -- 
 -- Calls rados_aio_write:
--- http://ceph.com/docs/master/rados/api/librados/#rados_aio_write
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_aio_write>
 asyncWrite :: IOContext
               -> Completion
               -> B.ByteString
@@ -174,14 +229,23 @@ asyncWrite ioctx completion oid offset bs =
             ioctxt_ptr c_oid rados_completion_t_ptr c_buf c_len c_offset
         return $ fromIntegral n
 -- |
+--
 -- From right to left, this function reads as:
--- "Write ByteString bytes to Word64 offset at oid ByteString within this
--- IOContext"
+-- Write ByteString buffer to Word64 offset at ByteString oid within this
+-- IOContext
+--
+--
+-- Usage is the same as asyncWrite, without a context.
+--
+-- @
+-- ...
+-- n <- syncWrite c \"oid\" 42 \"written at offset fourty-two\"
+-- @
 --
 -- Returns the number of bytes written.
 -- 
 -- Calls rados_aio_write:
--- http://ceph.com/docs/master/rados/api/librados/#rados_write
+-- <http://ceph.com/docs/master/rados/api/librados/#rados_write>
 syncWrite :: IOContext
          -> B.ByteString
          -> Word64
@@ -200,7 +264,16 @@ syncWrite ioctx oid offset bs =
 -- |
 -- Read length bytes into a ByteString, using context and oid.
 --
+-- There is no async read provided by this binding.
+--
 -- context -> oid -> length -> offset -> IO read_bytes
+--
+-- @
+-- ...
+-- bs <- syncRead ctx "oid" 100 42
+-- @
+--
+-- The above will place 100 bytes into bs, read from an offset of 42
 syncRead :: IOContext
     -> B.ByteString
     -> Word64
